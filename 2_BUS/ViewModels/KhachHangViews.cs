5f7648d9a104c1475ec5388850a496fd394f7f08
trang <trangph26466@fpt.edu.vn>
2022-11-24 00:09:20 +0700

    Da

--- 2_BUS/ViewModels/KhachHangViews.cs
+++ 2_BUS/ViewModels/KhachHangViews.cs
@@ -8,12 +8,10 @@ namespace _2_BUS.ViewModels
     {
         public Guid Id { get; set; }
         public string Ma { get; set; }
-        public string Ho { get; set; }
-        public string TenDem { get; set; }
         public string Ten { get; set; }
         public string SDT { get; set; }
         public string DiaChi { get; set; }
-        public string ThanhPho { get; set; }
+       public string KhungGio { get; set; }
         public int TrangThai { get; set; }
     }
 }
--- 2_BUS/ViewModels/XeViews.cs
+++ 2_BUS/ViewModels/XeViews.cs
@@ -12,7 +12,10 @@ namespace _2_BUS.ViewModels
         public string BienSo { get; set; }
         public int TrangThai { get; set; }
         public string TenKH { get; set; }
-
+        //public string DiaChiKH { get; set; }
+        //public int TrangThaiKH { get; set; }
+        //public string SDT { get; set; }
+        //public string KhungGio { get; set; }
 
     }
 }
